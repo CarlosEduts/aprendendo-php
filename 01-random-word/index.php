1f58a8e0dcfc4354e4815f2d054e153d02a8b178
Carlos Eduardo <131536369+CarlosEduts@users.noreply.github.com>
2025-01-22 15:27:33 +0000

    fix: Adicionando todos os exercícios já feitos

---
+++ 01-random-word/index.php
@@ -0,0 +1,54 @@
+<?php
+$apiUrl = "https://random-word-api.herokuapp.com/word";
+
+// Fazendo a requisição GET & Convertendo a resposta JSON para array PHP
+$response = file_get_contents($apiUrl);
+$wordArray = json_decode($response, true);
+
+$word = "";
+
+if (!empty($wordArray)) {
+    $word = $wordArray[0];
+} else {
+    $word = "Error to get word";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn new word</title>
+
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        .container {
+            background-color: <?php echo "rgb(" . rand(0, 255) . "," . rand(0, 255) . "," . rand(0, 255) . ")"; ?>;
+            width: 100%;
+            height: 100dvh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        h1 {
+            font-size: 3.5rem;
+            opacity: 0.8;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container" onclick="window.location.reload()">
+        <h1><?php echo $word; ?></h1>
+    </div>
+</body>
+
+</html>
---
+++ 02-random-motivational-frases/index.php
@@ -0,0 +1,53 @@
+<?php
+
+// Requisitando dados e transformando em json
+$apiURL = "https://zenquotes.io/api/random";
+$response = file_get_contents($apiURL);
+$fraseDataArray = json_decode($response, true);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Random motivational frase</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        .container {
+            background: #fff;
+            background: linear-gradient(90deg,
+                    <?php echo "rgb(" . rand(0, 255) . "," . rand(0, 255) . "," . rand(0, 255) . ")"; ?> 0%,
+                    <?php echo "rgb(" . rand(0, 255) . "," . rand(0, 255) . "," . rand(0, 255) . ")"; ?> 100%);
+            width: 100%;
+            height: 100dvh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            padding: 1rem;
+        }
+
+        h1 {
+            text-align: center;
+            font-size: 3rem;
+            opacity: 0.8;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container" onclick="window.location.reload()">
+        <h1> <?php echo $fraseDataArray[0]["q"]; ?> </h1>
+        <p> <?php echo $fraseDataArray[0]["a"]; ?> </p>
+    </div>
+</body>
+
+</html>
